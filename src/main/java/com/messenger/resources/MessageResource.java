b032752718e14a37d7bd205bb544bb2daeeab5c0
Shishir Thapa <shishirthapa1@gamil.com>
2018-08-26 18:29:34 +0545

    created api to get single message

--- src/main/java/com/messenger/resources/MessageResource.java
+++ src/main/java/com/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.messenger.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -18,5 +19,12 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessage(){
         return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long id){
+        return messageService.getMessage(id);
     }
 }
