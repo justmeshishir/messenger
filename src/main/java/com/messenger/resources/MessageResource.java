308590bd9ddc32ebe76ef6d3f44a9dcc586655d0
Shishir Thapa <shishirthapa1@gamil.com>
2018-08-26 21:10:08 +0545

    removed Produces n Cosumes annotation from each method to top level annotation

--- src/main/java/com/messenger/resources/MessageResource.java
+++ src/main/java/com/messenger/resources/MessageResource.java
@@ -9,28 +9,25 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("messages")
+@Consumes(MediaType.APPLICATION_JSON) //Accepts JSON Data
+@Produces(MediaType.APPLICATION_JSON) //Returns JSON Data
 public class MessageResource {
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessage(){
         return messageService.getAllMessages();
     }
 
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON) //Accepts JSON Data
-    @Produces(MediaType.APPLICATION_JSON) //Returns JSON Data
     public Message addMessage(Message message){
         return messageService.addMessage(message);
     }
 
     @PUT
     @Path("{messageId}")
-    @Consumes(MediaType.APPLICATION_JSON) //Accepts JSON Data
-    @Produces(MediaType.APPLICATION_JSON) //Returns JSON Data
     public Message updateMessage(@PathParam("messageId") long id, Message message) {
         message.setId(id);
         return messageService.updateMessage(message);
@@ -38,7 +35,6 @@ public class MessageResource {
 
     @DELETE
     @Path("{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void removeMessage(@PathParam("messageId") long id){
         messageService.removeMessage(id);
     }
@@ -46,7 +42,6 @@ public class MessageResource {
 
     @GET
     @Path("{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long id){
         return messageService.getMessage(id);
     }
