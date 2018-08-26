5a999f70a2728637e9c69147bee12840af2160d7
Shishir Thapa <shishirthapa1@gamil.com>
2018-08-26 18:11:24 +0545

    created message service to add, update, remove, select messages

--- src/main/java/com/messenger/service/MessageService.java
+++ src/main/java/com/messenger/service/MessageService.java
@@ -1,18 +1,41 @@
 package com.messenger.service;
 
+import com.messenger.database.DatabaseClass;
 import com.messenger.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
 
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
+
     public List<Message> getAllMessages(){
-        Message m1 = new Message(1L,"Hello Bailey", "ccr");
-        Message m2 = new Message(2L,"Hello LF", "ccr");
-        List<Message> list = new ArrayList<>();
-        list.add(m1);
-        list.add(m2);
-        return list;
+        return new ArrayList<Message>(messages.values());
     }
+
+    public Message getMessage(long id){
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message){
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message){
+        if(message.getId() <= 0){
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id){
+        return messages.remove(id);
+    }
+
+
 }
